2f43f161dd616f9e9c26f4362af00aa4efdc0bf6
liambarstad <liambarstad@gmail.com>
2017-08-01 09:08:34 -0600

    Pantry exists and initializes with empty stock hash

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,17 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_stock_initializes_empty
+    pantry = Pantry.new
+
+    assert_instance_of Hash, pantry.stock
+    assert pantry.stock.empty?
+  end
 
 end
